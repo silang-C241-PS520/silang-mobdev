195803cc979bb8f1b028e3167d8a6785ddeb0b20
alfnsnff <alfnsnff@gmail.com>
2024-05-31 19:48:29 +0700

    Add: Main Background, TranslateActivityte

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.dicoding.silang"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.dicoding.silang"
         minSdk = 24
@@ -41,7 +45,12 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity:1.9.0")
+    implementation("androidx.compose.runtime:runtime-android:1.6.7")
+    implementation("androidx.compose.material3:material3-android:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
