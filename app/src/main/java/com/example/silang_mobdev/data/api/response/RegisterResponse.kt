9d18c5fd9a6be057db5cfb1e4f358a8ccf944483
FagihAkram <faqih.akram07@gmail.com>
2024-06-02 22:13:29 +0700

    Add singup activity

---
+++ app/src/main/java/com/example/silang_mobdev/data/api/response/RegisterResponse.kt
@@ -0,0 +1,6 @@
+package com.example.silang_mobdev.data.api.response
+
+data class RegisterResponse(
+    val error: Boolean? = null,
+    val message: String? = null
+)
--- app/src/main/java/com/example/silang_mobdev/data/api/retrofit/ApiService.kt
+++ app/src/main/java/com/example/silang_mobdev/data/api/retrofit/ApiService.kt
@@ -3,11 +3,22 @@ package com.example.silang_mobdev.data.api.retrofit
 
 
 import com.example.silang_mobdev.data.api.response.LoginResponse
+import com.example.silang_mobdev.data.api.response.RegisterResponse
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface ApiService {
+
+    @FormUrlEncoded
+    @POST("register")
+    suspend fun register(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): RegisterResponse
+
+
     @FormUrlEncoded
     @POST("login")
     suspend fun login(
