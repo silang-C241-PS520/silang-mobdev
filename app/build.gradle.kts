a07e138fba47fd9feebfa8bd4ac706d0de9dc6ef
alfnsnff <alfnsnff@gmail.com>
2024-06-02 15:04:03 +0700

    Auth: Add Login Acitivity XML, Complete Functionality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,5 +53,12 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation ("com.google.android.material:material:1.9.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
 }
---
+++ app/src/main/java/com/example/silang_mobdev/data/pref/UserModel.kt
@@ -0,0 +1,7 @@
+package com.example.silang_mobdev.data.pref
+
+data class UserModel(
+    val email: String,
+    val token: String,
+    val isLogin: Boolean = false
+)
