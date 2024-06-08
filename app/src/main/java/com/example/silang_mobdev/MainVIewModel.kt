3953badb12bb8405d5eee65f0d0031589b685517
alfnsnff <alfnsnff@gmail.com>
2024-06-08 18:55:42 +0700

    Modify: Restructure the project navigation & Redesign ui

--- app/src/main/java/com/example/silang_mobdev/MainVIewModel.kt
+++ app/src/main/java/com/example/silang_mobdev/MainVIewModel.kt
@@ -3,13 +3,21 @@ package com.example.silang_mobdev
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.silang_mobdev.data.Repository
 import com.example.silang_mobdev.data.pref.UserModel
+import kotlinx.coroutines.launch
 
 class MainViewModel(private val repository: Repository) : ViewModel() {
 
     fun getSession(): LiveData<UserModel> {
         return repository.getSession().asLiveData()
+    }
+
+    fun logout() {
+        viewModelScope.launch {
+            repository.logout()
+        }
     }
 
 }
--- app/src/main/java/com/example/silang_mobdev/ViewModelFactory.kt
+++ app/src/main/java/com/example/silang_mobdev/ViewModelFactory.kt
@@ -5,8 +5,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.silang_mobdev.data.Repository
 import com.example.silang_mobdev.data.di.Injection
-import com.example.silang_mobdev.ui.history.HistoryViewModel
-import com.example.silang_mobdev.ui.home.HomeViewModel
 import com.example.silang_mobdev.ui.login.LoginViewModel
 
 class ViewModelFactory(private val repository: Repository) :
@@ -21,13 +19,6 @@ class ViewModelFactory(private val repository: Repository) :
             modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
                 LoginViewModel(repository) as T
             }
-            modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
-                HomeViewModel(repository) as T
-            }
-            modelClass.isAssignableFrom(HistoryViewModel::class.java) -> {
-                HistoryViewModel(repository) as T
-            }
-
             else -> throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
         }
     }
