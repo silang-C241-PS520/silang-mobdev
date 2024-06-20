0ceae9b5c2d18eb975f87e26c390cd68e698c02d
alfnsnff <alfnsnff@gmail.com>
2024-06-20 14:31:07 +0700

    Add: Copy to clipboard on Translate History

--- app/src/main/java/com/example/silang_mobdev/HistoryAdapter.kt
+++ app/src/main/java/com/example/silang_mobdev/HistoryAdapter.kt
@@ -1,8 +1,17 @@
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.view.LayoutInflater
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.silang_mobdev.R
 import com.example.silang_mobdev.data.api.response.TranslationResponse
 import com.example.silang_mobdev.databinding.RecyclerItemBinding
 import java.text.SimpleDateFormat
@@ -33,6 +42,12 @@ class HistoryAdapter : ListAdapter<TranslationResponse, HistoryAdapter.HistoryVi
     }
 
     class HistoryViewHolder(private val binding: RecyclerItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.moreOptions.setOnClickListener { view ->
+                showPopupMenu(view, binding)
+            }
+        }
+
         fun bind(translation: TranslationResponse) {
             binding.itemTitle.text = translation.translation_text
 
@@ -54,6 +69,33 @@ class HistoryAdapter : ListAdapter<TranslationResponse, HistoryAdapter.HistoryVi
             } catch (e: Exception) {
                 "Invalid Date"
             }
+        }
+
+        private fun showPopupMenu(view: View, binding: RecyclerItemBinding) {
+            val popup = PopupMenu(view.context, view)
+            val inflater: MenuInflater = popup.menuInflater
+            inflater.inflate(R.menu.overflow_menu, popup.menu)
+            popup.setOnMenuItemClickListener { menuItem ->
+                handleMenuItemClick(menuItem, binding)
+            }
+            popup.show()
+        }
+
+        private fun handleMenuItemClick(menuItem: MenuItem, binding: RecyclerItemBinding): Boolean {
+            return when (menuItem.itemId) {
+                R.id.context_menu_copy -> {
+                    copyTextToClipboard(binding.itemTitle.text.toString(), itemView.context)
+                    true
+                }
+                else -> false
+            }
+        }
+
+        private fun copyTextToClipboard(text: String, context: Context) {
+            val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("Result Text", text)
+            clipboard.setPrimaryClip(clip)
+            Toast.makeText(context, "Text copied to clipboard", Toast.LENGTH_SHORT).show()
         }
     }
 }
